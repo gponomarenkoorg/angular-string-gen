84c788852d3ae6dd2d4fe125b97019b037a24fc4
Grygoriy Ponomarenko <profit4fam@gmail.com>
2021-08-17 16:30:27 +0300

    component update - learning purpose

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NgWebsite
+# Angular generator
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.0.
 
@@ -25,3 +25,14 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## Test Task
+<pre>
+  Ваша задача создать генератор случайных строк. Каждое значение должно состоять из 5 символов.
+Настройте генерацию и отображение значений через каждые 3 секунды.
+При отображения отображении значения пользователю выполните следующие условия:
+1) если созданное значение является палиндромом отобразите его красным цветом;
+2) если созданное значение состоит только из цифр отобразите его синим цветом;
+3) если созданное содержит 0 не отображайте его вовсе.
+При настройке отображения через каждые 3 секунды используйте RxJs.
+</pre>
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,28 +1,31 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  template: `
-  <div class="card">
-    <h2>Card component</h2>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque, totam.</p>
-  </div>
-  `,
-  styles: [
-    `
-      .card {
-        padding: 0.5rem 1rem;
-        border: 1px dashed #ccc;
-        margin-bottom: 1rem;
-      }
-
-      h2 {
-        margin-bottom: 1rem;
-      }
-    `
-  ]
-  // templateUrl: './card.component.html',
-  // styleUrls: ['./card.component.css'],
-
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
 })
-export class CardComponent {}
+export class CardComponent implements OnInit {
+  title = 'My Card Title'
+  text = 'My Sample Text'
+
+  imgUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+
+  number = 42
+
+  array = [1, 1, 2, 3, 5, 8, 13]
+
+  obj = {name: 'Grygoriy', info: {age: 39, job: 'FE'}}
+
+  ngOnInit() {
+    console.log('ngOnInit');
+
+    setTimeout(() => {
+      this.imgUrl = 'https://cdn.iconscout.com/icon/free/png-256/vue-282497.png'
+    }, 3000)
+  }
+
+  getInfo(): string {
+    return 'This is my info'
+  }
+}
